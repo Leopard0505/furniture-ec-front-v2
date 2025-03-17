7ee6dc848f336f85ce4c06b96a55c1e67799ed3a
Leopard0505 <k.9975hyo5@gmail.com>
2025-03-17 23:34:00 +0900

    add: Cookieを使用できるようにした

---
+++ src/constants/cookie.ts
@@ -0,0 +1,6 @@
+export const config = {
+  path: "/",
+  maxAge: 7 * 24 * 60 * 60,
+  secure: true,
+  sameSite: true,
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
+import { CookiesProvider } from 'react-cookie'
 import './assets/scss/_reset.scss'
 import './index.css'
 import { router } from './router'
+import { config } from './constants/cookie'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <CookiesProvider defaultSetOptions={config}>
+      <RouterProvider router={router} />
+    </CookiesProvider>
   </StrictMode>,
 )
