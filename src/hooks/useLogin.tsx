c051a7e0dce7ba4bab7934e3d0d0194277afaf11
Leopard0505 <k.9975hyo5@gmail.com>
2025-03-17 19:02:44 +0900

    #11 add: ログインフックにログイン処理を実装した

--- src/hooks/useLogin.tsx
+++ src/hooks/useLogin.tsx
@@ -1,13 +1,27 @@
 import { useEffect, useState } from "react";
-
+import { authLoginApi } from "../api/auth";
+import { usePageNavigate } from "./usePageNavigate";
 
 export const useLogin = () => {
   const [email, setEmail] = useState<string>('');
   const [password, setPassword] = useState<string>('');
 
-  const login = (): Promise<void> => {
-    console.log('login', { email, password });
-    return Promise.resolve();
+  const { toTopNavigate } = usePageNavigate();
+
+  const login = async (): Promise<void> => {
+    try {
+      console.log('login', { email, password });
+
+      const response = await authLoginApi(email, password);
+      console.log('reponse', response);
+
+      // TODO: 認証情報をstoreに保持する
+
+      toTopNavigate();
+    } catch (error) {
+      alert(error);
+      // TODO: エラーハンドリング
+    }
   }
 
   // developmentモードで値を注入
