641dcae452278ec0a87099cf8ab94fa9859f8903
Leopard0505 <k.9975hyo5@gmail.com>
2025-03-18 02:44:19 +0900

    fix: レイアウトのルートを変更した

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -1,10 +1,13 @@
 //Outletをインポート
 import { Outlet } from 'react-router-dom';
 
-import Header from '../../components/Header/Header';
-import Footer from '../../components/Footer/Footer';
+import Header from '../components/Header/Header';
+import Footer from '../components/Footer/Footer';
+import { useMode } from '../hooks/useMode';
 
 export default function Layout() {
+
+  useMode();
 
   return (
     <>
