cbed48b574976ca2265071155817ee9e040ca318
Leopard0505 <48083491+Leopard0505@users.noreply.github.com>
2025-03-19 01:09:39 +0900

    [Feature/issue-4]: 会員登録画面にフォームバリデーションを導入する (#26)
    * #4 add: 会員登録画面にバリデーション処理を追加した
    
    * #4 fix: 会員登録処理の引数にバリデーション結果の値を渡すように修正した

--- src/components/SignupPortal/SignupPortal.tsx
+++ src/components/SignupPortal/SignupPortal.tsx
@@ -1,10 +1,13 @@
 
+import { FormProvider, useForm } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
 import { Button } from '../Button/Button';
 import { InputField } from '../InputField/InputField';
 import { InputPasswordField } from '../InputPasswordField/InputPasswordField';
 import { SectionTitle } from '../SectionTitle/SectionTitle';
 
 import styles from './SignupPortal.module.scss';
+import { FormInputs, signupSchema } from './schema';
 
 interface SignupPortalProps {
   email: string;
@@ -18,34 +21,70 @@ interface SignupPortalProps {
   ding: string;
   buildname: string;
   roomname: string;
-  onChange: (args: { email?: string, password?: string, name?: string, namekana?: string, phonenumber?: string, postcode?: string, prefecture?: string, municipality?: string, ding?: string, buildname?: string, roomname?: string }) => void;
-  signup: () => Promise<void>;
+  signup: (
+    username: string,
+    password: string,
+    name: string,
+    namekana: string,
+    phonenumber: string,
+    postcode: string,
+    prefecture: string,
+    municipality: string,
+    ding: string,
+    buildname: string,
+    roomname: string,
+  ) => Promise<void>;
 }
 
 export function SignupPortal(props: SignupPortalProps) {
+  const useFormMethods = useForm({
+    resolver: zodResolver(signupSchema),
+    defaultValues: {
+      username: props.email,
+      password: props.password,
+    }
+  });
+
+  const onSubmit = async (data: FormInputs) => {
+    await props.signup(
+      data.username,
+      data.password,
+      data.name,
+      data.namekana,
+      data.phonenumber,
+      data.postcode,
+      data.prefecture,
+      data.municipality,
+      data.ding,
+      data.buildname,
+      data.roomname
+    );
+  }
 
   return (
     <div className={styles.signup__portal}>
       <SectionTitle text="会員登録" />
-      <div>
-        <InputField text='メールアドレス' value={props.email} onChange={(value) => props.onChange({ email: value })} />
-        <InputPasswordField text='パスワード' value={props.password} onChange={(value) => props.onChange({ password: value })} />
-        <InputField text='氏名' value={props.name} onChange={(value) => props.onChange({ name: value })} />
-        <InputField text='氏名（フリガナ）' value={props.namekana} onChange={(value) => props.onChange({ namekana: value })} />
-        <InputField text='電話番号' value={props.phonenumber} onChange={(value) => props.onChange({ phonenumber: value })} />
-        <InputField text='郵便番号（半角数字）' value={props.postcode} onChange={(value) => props.onChange({ postcode: value })} />
-        <InputField text='都道府県' value={props.prefecture} onChange={(value) => props.onChange({ prefecture: value })} />
-        <InputField text='市区町村' value={props.municipality} onChange={(value) => props.onChange({ municipality: value })} />
-        <InputField text='丁目・番地・号（数字は半角数字）' value={props.ding} onChange={(value) => props.onChange({ ding: value })} />
-        <InputField text='建物名' value={props.buildname} onChange={(value) => props.onChange({ buildname: value })} />
-        <InputField text='部屋番号（数字は半角数字）' value={props.roomname} onChange={(value) => props.onChange({ roomname: value })} />
-        <div className={styles.signup__portal__agree}>
-          <a className={styles.signup__portal__agree_link} href="#">規約および個人情報保護方針への同意が必要です。</a>
-        </div>
-        <div>
-          <Button text='同意して次へ' onClick={() => props.signup()} />
-        </div>
-      </div>
+      <FormProvider {...useFormMethods}>
+        <form onSubmit={useFormMethods.handleSubmit(onSubmit)}>
+          <InputField label='メールアドレス' name='username' />
+          <InputPasswordField label='パスワード' name='password' />
+          <InputField label='氏名' name='name' />
+          <InputField label='氏名（フリガナ）' name='namekana' />
+          <InputField label='電話番号' name='phonenumber' />
+          <InputField label='郵便番号（半角数字）' name='postcode' />
+          <InputField label='都道府県' name='prefecture' />
+          <InputField label='市区町村' name='municipality' />
+          <InputField label='丁目・番地・号（数字は半角数字）' name='ding' />
+          <InputField label='建物名' name='buildname' />
+          <InputField label='部屋番号（数字は半角数字）' name='roomname' />
+          <div className={styles.signup__portal__agree}>
+            <a className={styles.signup__portal__agree_link} href="#">規約および個人情報保護方針への同意が必要です。</a>
+          </div>
+          <div>
+            <Button type='submit' text='同意して次へ' />
+          </div>
+        </form>
+      </FormProvider>
     </div>
   );
 }
