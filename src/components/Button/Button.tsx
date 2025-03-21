fc1d5ad9ffe69f8292424e40c88be65f677aded6
Leopard0505 <k.9975hyo5@gmail.com>
2025-03-21 15:22:27 +0900

    fix: ボタンタイプを追加

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -3,14 +3,15 @@ import styles from './Button.module.scss';
 
 interface ButtonProps {
   text: string;
+  type?: "button" | "submit" | "reset" | undefined;
   onClick?: () => void;
 }
 
 export function Button(props: ButtonProps) {
-
   return (
     <button
       className={styles.button}
+      type={props.type}
       onClick={props.onClick}>
       {props.text}
     </button>
