dc10ac78d1623e1831019930933bcfc574a823e5
Leopard0505 <k.9975hyo5@gmail.com>
2025-03-26 22:15:34 +0900

    #28 chore: 大まかな枠組みを作った

--- src/routes/items/Items.tsx
+++ src/routes/items/Items.tsx
@@ -1,7 +1,18 @@
+import { SectionTitle } from "../../components/SectionTitle/SectionTitle";
 
 export default function Items() {
 
   return (
-    <div>Items</div>
+    <main>
+      <div>条件検索</div>
+      <div>
+        <div>並び替え</div>
+        <div>
+          <SectionTitle text={'カテゴリ名'} />
+          <div>商品一覧</div>
+          <div>ページネーション</div>
+        </div>
+      </div>
+    </main>
   )
 }
