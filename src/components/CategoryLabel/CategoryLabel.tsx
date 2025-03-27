9863a56dffd00de85ceb2d5b5c8fab9d5418f781
Leopard0505 <k.9975hyo5@gmail.com>
2025-03-28 01:10:56 +0900

    #28 add: CategoryLabelコンポーネントを追加した

---
+++ src/components/CategoryLabel/CategoryLabel.tsx
@@ -0,0 +1,19 @@
+
+import styles from './CategoryLabel.module.scss';
+
+interface CategoryLabelProps {
+  text: string;
+  onClick?: () => void;
+}
+
+export function CategoryLabel(props: CategoryLabelProps) {
+
+  return (
+    <span
+      className={styles.category__label}
+      onClick={props.onClick}
+    >
+      {props.text}
+    </span>
+  );
+}
