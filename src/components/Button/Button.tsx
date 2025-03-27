a78002c72da7809ead7152d0ae759a36803fa069
Leopard0505 <k.9975hyo5@gmail.com>
2025-03-27 19:13:30 +0900

    #28 fix: Buttonコンポーネントが白色を指定できるように修正した

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,20 @@
 
+import classNames from 'classnames';
 import styles from './Button.module.scss';
 
 interface ButtonProps {
   text: string;
   type?: "button" | "submit" | "reset" | undefined;
+  white?: boolean;
   onClick?: () => void;
 }
 
 export function Button(props: ButtonProps) {
+  const convertClassName = classNames(styles.button, props.white ? styles.white : null);
+
   return (
     <button
-      className={styles.button}
+      className={convertClassName}
       type={props.type}
       onClick={props.onClick}>
       {props.text}
