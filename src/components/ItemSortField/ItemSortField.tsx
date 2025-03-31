a1d8ab12e475314c9dc4a5dbab9155a43d308ede
Leopard0505 <k.9975hyo5@gmail.com>
2025-03-31 23:40:29 +0900

    #28 fix: キーボードイベントに対応した

--- src/components/ItemSortField/ItemSortField.tsx
+++ src/components/ItemSortField/ItemSortField.tsx
@@ -13,18 +13,52 @@ export function ItemSortField() {
 
   return (
     <div className={styles.item__sort__field}>
-      <div className={styles.item__sort__field__container} onClick={handleClickAccordion} onKeyUp={(e: KeyboardEvent) => handleEnterKey(e, handleClickAccordion)}>
+      <div
+        className={styles.item__sort__field__container}
+        onClick={handleClickAccordion}
+        onKeyUp={(e: KeyboardEvent) => handleEnterKey(e, handleClickAccordion)}
+      >
         <div>並び替え</div>
         <BsFilterLeft className={styles.item__sort__field__container__icon} />
       </div>
       <div className={convertClassName}>
         <div className={styles.item__sort__field__list__inner}>
           <div className={styles.item__sort__field__list__inner__row}>
-            <div className={styles.item__sort__field__list__item} onClick={() => handleSetItemSort('price', 'asc')}>価格：安い順</div>
-            <div className={styles.item__sort__field__list__item} onClick={() => handleSetItemSort('price', 'desc')}>価格：高い順</div>
-            <div className={styles.item__sort__field__list__item} onClick={() => handleSetItemSort('created_at', 'desc')}>新着商品</div>
-            <div className={styles.item__sort__field__list__item} onClick={() => handleSetItemSort('review_count', 'desc')}>レビュー数が多い順</div>
-            <div className={styles.item__sort__field__list__item} onClick={() => handleSetItemSort('review_average', 'desc')}>レビュー評価：高い順</div>
+            <div
+              className={styles.item__sort__field__list__item}
+              onClick={() => handleSetItemSort('price', 'asc')}
+              onKeyUp={(e: KeyboardEvent) => handleEnterKey(e, () => handleSetItemSort('price', 'asc'))}
+            >
+              価格：安い順
+            </div>
+            <div
+              className={styles.item__sort__field__list__item}
+              onClick={() => handleSetItemSort('price', 'desc')}
+              onKeyUp={(e: KeyboardEvent) => handleEnterKey(e, () => handleSetItemSort('price', 'desc'))}
+            >
+              価格：高い順
+            </div>
+            <div
+              className={styles.item__sort__field__list__item}
+              onClick={() => handleSetItemSort('created_at', 'desc')}
+              onKeyUp={(e: KeyboardEvent) => handleEnterKey(e, () => handleSetItemSort('created_at', 'desc'))}
+            >
+              新着商品
+            </div>
+            <div
+              className={styles.item__sort__field__list__item}
+              onClick={() => handleSetItemSort('review_count', 'desc')}
+              onKeyUp={(e: KeyboardEvent) => handleEnterKey(e, () => handleSetItemSort('review_count', 'desc'))}
+            >
+              レビュー数が多い順
+            </div>
+            <div
+              className={styles.item__sort__field__list__item}
+              onClick={() => handleSetItemSort('review_average', 'desc')}
+              onKeyUp={(e: KeyboardEvent) => handleEnterKey(e, () => handleSetItemSort('review_average', 'desc'))}
+            >
+              レビュー評価：高い順
+            </div>
           </div>
           <div className={styles.item__sort__field__list__button}>
             <Button text='クリア' white onClick={handleClearSort} />
