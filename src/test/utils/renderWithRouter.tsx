0e2102e03c93eb85c6f141670a56f25a7f2885f8
Leopard0505 <k.9975hyo5@gmail.com>
2025-04-10 18:33:26 +0900

    #29 test: ストアが含まれててもテストが通るように修正した

--- src/test/utils/renderWithRouter.tsx
+++ src/test/utils/renderWithRouter.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { BrowserRouter } from 'react-router';
+import { Provider } from 'react-redux';
+import { store } from '../../stores/store';
 
 export const renderWithRouter = (component: React.ReactElement) => {
   return render(
-    <BrowserRouter>
-      {component}
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        {component}
+      </BrowserRouter>
+    </Provider>
   );
 };
