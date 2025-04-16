5271e3349e8c558e117df5bbe1bf530619f898a6
Leopard0505 <k.9975hyo5@gmail.com>
2025-04-16 22:38:42 +0900

    #80 add: お気に入り情報管理のストアを追加した

--- src/stores/store.ts
+++ src/stores/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { reducer as cartReducer } from "./cart/cartSlice";
+import { reducer as favoriteReducer } from "./favorite/favoriteSlice";
 
 export const store = configureStore({
   reducer: {
     cart: cartReducer,
+    favorite: favoriteReducer,
   },
 });
 
