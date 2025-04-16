7181a80de78ca72fd27fc902b0ce283b60ce9cc7
Leopard0505 <k.9975hyo5@gmail.com>
2025-04-17 01:14:29 +0900

    #80 fix: オススメ商品コンポーネントを追加しオススメ商品コンポーネントを使うように統一した

--- src/routes/me/Favorites.tsx
+++ src/routes/me/Favorites.tsx
@@ -1,9 +1,11 @@
 import { FavoriteItemList } from "../../components/FavoriteItemList/FavoriteItemList";
+import { RecommendItemListForYou } from "../../components/RecommendItemListForYou/RecommendItemListForYou";
 
 export function MeFavorites() {
   return (
     <div>
       <FavoriteItemList />
+      <RecommendItemListForYou />
     </div>
   );
 }
