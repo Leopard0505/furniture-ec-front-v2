c8156414f26b024adecf859f379da9b49bccaca5
Leopard0505 <k.9975hyo5@gmail.com>
2025-04-22 00:09:42 +0900

    #31 fix: レイアウトを修正した

--- src/routes/Checkout/Checkout.tsx
+++ src/routes/Checkout/Checkout.tsx
@@ -1,17 +1,24 @@
+import { CartSummary } from "../../components/CartSummary/CartSummary";
 import { CheckoutCouponAvailable } from "../../components/CheckoutCouponAvailable/CheckoutCouponAvailable";
 import { CheckoutDeliveryAddress } from "../../components/CheckoutDeliveryAddress/CheckoutDeliveryAddress";
 import { CheckoutDeliveryDateTime } from "../../components/CheckoutDeliveryDateTime/CheckoutDeliveryDateTime";
 import { CheckoutPaymentMethod } from "../../components/CheckoutPaymentMethod/CheckoutPaymentMethod";
 import { CheckoutPointAvailable } from "../../components/CheckoutPointAvailable/CheckoutPointAvailable";
+import styles from "./Checkout.module.scss";
 
 export function Checkout() {
   return (
-    <div>
-      <CheckoutDeliveryAddress />
-      <CheckoutDeliveryDateTime />
-      <CheckoutPaymentMethod />
-      <CheckoutPointAvailable />
-      <CheckoutCouponAvailable />
+    <div className={styles.wrapper}>
+      <div>
+        <CheckoutDeliveryAddress />
+        <CheckoutDeliveryDateTime />
+        <CheckoutPaymentMethod />
+        <CheckoutPointAvailable />
+        <CheckoutCouponAvailable />
+      </div>
+      <div className={styles.summary}>
+        <CartSummary />
+      </div>
     </div>
   );
 }
