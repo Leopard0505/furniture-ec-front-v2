84dc342e405ec06ddac58dcae0ab6d0c7a0aadfb
Leopard0505 <k.9975hyo5@gmail.com>
2025-05-23 14:50:29 +0900

    fix: プロフィールアイコンを修正

--- src/components/HeaderTextProfileButton/HeaderTextProfileButton.tsx
+++ src/components/HeaderTextProfileButton/HeaderTextProfileButton.tsx
@@ -1,12 +1,13 @@
-import { MdOutlineAccountCircle } from "react-icons/md";
+import { MdSwitchAccount } from "react-icons/md";
 import { HeaderTextIconButton } from "../HeaderTextIconButton/HeaderTextIconButton";
 
 import styles from './HeaderTextProfileButton.module.scss';
+import { PATH } from "../../constants/path";
 
 export function HeaderTextProfileButton() {
   return (
-    <HeaderTextIconButton to="/profile">
-      <MdOutlineAccountCircle className={styles.icon} />
+    <HeaderTextIconButton to={PATH.ME()}>
+      <MdSwitchAccount className={styles.icon} />
     </HeaderTextIconButton>
   );
 }
--- src/routes/me/Me.tsx
+++ src/routes/me/Me.tsx
@@ -1,7 +1,13 @@
+import { Link } from "react-router";
+import { PATH } from "../../constants/path";
 
 export default function Me() {
 
   return (
-    <div>Me</div>
+    <div>
+      <Link to={PATH.ME_ORDERS()}>Orders</Link>
+      <br />
+      <Link to={PATH.ME_FAVORITES()}>Favorites</Link>
+    </div>
   )
 }
